4dd69b3bd83128a11f026d3b834d8bec55aff111
Pete Jeffryes <pete@flowhub.co>
2017-09-03 18:03:44 -0600

    cleaned up dao, added auth to currency routes

--- src/server/routes/currency.js
+++ src/server/routes/currency.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../dao/auth');
 const currency = require('../dao/currency');
 
-router.get('/getLowestRate/:currencyCode', currency.getLowestRate);
+router.get('/getLowestRate/:currencyCode', auth.checkAuthentication, currency.getLowestRate);
 
 module.exports = router;
